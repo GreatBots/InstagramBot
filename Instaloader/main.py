6f017b3c129caa6373eb81541a94b9434a80423a
Vivek <vivektp2006@gmail.com>
2024-02-24 11:26:01 +0530

    Update main.py

--- Instaloader/main.py
+++ Instaloader/main.py
@@ -2,17 +2,16 @@ import os
 import re
 import asyncio
 import shutil
-from config import INSTA_USERNAME, INSTA_PASSWORD  # Assuming your config file is named 'config.py'
+from Config import INSTA_USERNAME, INSTA_PASSWORD
 from pyrogram import Client, filters
 from .database.users_sql import get_info
 
-app = Client("my_account")
 
-@app.on_message(filters.private & ~filters.command("start"))
+@Client.on_message(filters.private & ~filters.regex(r'^/'))
 async def main(_, msg):
     if 'instagram.com' not in msg.text:
         return
-    status = await msg.reply_text('Please Wait...', quote=True)
+    status = await msg.reply('Please Wait...', quote=True)
     pattern = re.compile(r'^(https?:[/][/])?(www\.)?instagram.com[/](p|reel)[/]([A-Za-z0-9-_]+)')
     try:
         matches = pattern.search(msg.text)
@@ -25,7 +24,7 @@ async def main(_, msg):
             command = f"instaloader --no-metadata-json -l {username} -p {password} -- -{post_id}"
         else:
             command = f"instaloader --no-metadata-json -- -{post_id}"
-        proc = await asyncio.create_subprocess_shell(
+        proc = await asyncio.subprocess.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
@@ -37,7 +36,7 @@ async def main(_, msg):
         photos, videos, caption = post_prep(path)
         if not photos and not videos:
             await status.delete()
-            await msg.reply_text("No Such Instagram Post Exists.")
+            await msg.reply("No Such Instagram Post Exists.")
             return
         if len(photos+videos) == 1:
             if caption:
@@ -46,10 +45,10 @@ async def main(_, msg):
                 caption = "By @StarkBots"
             if photos:
                 for photo in photos:
-                    await msg.reply_photo(photo, caption=caption)
+                    await msg.reply_photo(photo, caption)
             if videos:
                 for video in videos:
-                    await msg.reply_video(video, caption=caption)
+                    await msg.reply_video(video, caption)
         else:
             if photos:
                 for photo in photos:
@@ -58,12 +57,13 @@ async def main(_, msg):
                 for video in videos:
                     await msg.reply_video(video)
             if caption:
-                await msg.reply_text(f"**POST CAPTION : **\n\n{caption} \n\nBy @StarkBots")
+                await msg.reply(f"**POST CAPTION : **\n\n{caption} \n\nBy @StarkBots")
         await status.delete()
         shutil.rmtree(path)
     except AttributeError:
         await status.delete()
-        await msg.reply_text(error)
+        await msg.reply(error)
+
 
 error = """
 Please send me a valid instagram post link.
@@ -72,17 +72,20 @@ It must be like one of the given below
 **Note** : To get profile picture of a account use "`/profile_pic instagram-username`". Link won't work.
 """
 
+
 def post_prep(path):
     if not os.path.isdir(path):
-        return [], [], None
+        return None, None, None
     files = os.listdir(path)
-    photos = [os.path.join(path, file) for file in files if file.endswith(".jpg")]
-    videos = [os.path.join(path, file) for file in files if file.endswith(".mp4")]
-    caption_file = [os.path.join(path, file) for file in files if file.endswith(".txt")]
+    photos = []
+    videos = []
     caption = ""
-    if caption_file:
-        with open(caption_file[0], "r") as f:
-            caption = f.read()
+    for file in files:
+        if file.endswith(".jpg"):
+            photos.append(path+'/'+file)
+        if file.endswith(".mp4"):
+            videos.append(path+'/'+file)
+        if file.endswith(".txt"):
+            with open(f"{path}/{file}") as f:
+                caption = f.read()
     return photos, videos, caption
-
-app.run()
