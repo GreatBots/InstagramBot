a4a324132796b9b8acfa6c8a92baff8360b52f07
Vivek <vivektp2006@gmail.com>
2024-02-24 11:46:22 +0530

    Update main.py

--- Instaloader/main.py
+++ Instaloader/main.py
@@ -5,7 +5,7 @@ import shutil
 from Config import INSTA_USERNAME, INSTA_PASSWORD
 from pyrogram import Client, filters
 from .database.users_sql import get_info
-
+import instaloader
 
 @Client.on_message(filters.private & ~filters.regex(r'^/'))
 async def main(_, msg):
@@ -16,22 +16,18 @@ async def main(_, msg):
     try:
         matches = pattern.search(msg.text)
         post_id = matches.group(4)
+        L = instaloader.Instaloader()
         username, password = await get_info(msg.from_user.id)
         if not username:
             username = INSTA_USERNAME
             password = INSTA_PASSWORD
         if username and password:
-            command = f"instaloader --no-metadata-json -l {username} -p {password} -- -{post_id}"
-        else:
-            command = f"instaloader --no-metadata-json -- -{post_id}"
-        proc = await asyncio.subprocess.create_subprocess_shell(
-            command,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await proc.communicate()
-        if "wrong password" in str(stderr).lower():
-            raise Exception('Wrong Instagram Password.')
+            L.load_session_from_file(username)
+        posts = instaloader.Profile.from_username(L.context, username).get_posts()
+        for post in posts:
+            if post.shortcode == post_id:
+                L.download_post(post, target= f"-{post_id}")
+                break
         path = f"-{post_id}"
         photos, videos, caption = post_prep(path)
         if not photos and not videos:
@@ -66,10 +62,10 @@ async def main(_, msg):
 
 
 error = """
-Please send me a valid instagram post link.
+Please send me a valid Instagram post link.
 It must be like one of the given below
 
-**Note** : To get profile picture of a account use "`/profile_pic instagram-username`". Link won't work.
+**Note** : To get a profile picture of an account, use "`/profile_pic instagram-username`". The link won't work.
 """
 
 
